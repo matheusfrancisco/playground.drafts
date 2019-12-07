d68e7f2e7957396f63d6f71d01b0b32aca80b271
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-12-07 11:25:44 -0300

    Add template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to remote-controlled-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/remote_controlled_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns remote-controlled-clj.core-test
+  (:require [clojure.test :refer :all]
+            [remote-controlled-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
