5f3d7d8c90d35db0bda7e8c52aa99fb376517352
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-12-08 22:26:17 -0300

    Add test to return new-board

---
+++ api/test/api/logic/board_test.clj
@@ -0,0 +1,11 @@
+(ns api.logic.board-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [api.logic.board :as board]))
+
+
+(facts "Be able to create an empty simulation space -an empty 50 x 50 grid"
+       (fact "Must be a 50x50 grid"
+             (board/new-board 50 50) => {:size-x 50 :size-y 50}))
+
+
