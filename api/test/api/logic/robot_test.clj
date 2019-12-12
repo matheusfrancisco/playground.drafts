caa83442f56b1d5d03195de053a55db9d20df479
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-12-11 23:24:31 -0300

    Add test to create a robot

---
+++ api/test/api/logic/robot_test.clj
@@ -0,0 +1,11 @@
+(ns api.logic.robot-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [api.logic.robot :as robot]))
+
+
+(facts "Be able to create and check position to robot"
+       (fact "Create a robot with id 1 position-x 1, position-y 2 in map anda direction up"
+             (robot/create-robot 1 {:x 1 :y 2} :up) => {:id 1 :type :robot :position {:x 1 :y 2} :direction :up}))
+
+
