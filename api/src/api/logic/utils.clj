3bf6a8773977d35ce77afb32f338ca2aabda4fd6
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-12-12 22:28:16 -0300

    Add function to create direction and orientation

--- api/src/api/logic/utils.clj
+++ api/src/api/logic/utils.clj
@@ -6,3 +6,26 @@
   "Create a new point"
   [position-x position-y]
   (->Point position-x position-y))
+
+(defonce orientation-sides
+  {:up    (create-point  0 -1)
+   :right (create-point  1 0)
+   :down  (create-point  0 1)
+   :left  (create-point -1 0)})
+
+(defrecord Direction [orientation position])
+
+(defn create-direction-oriented
+  "Create a  new direction"
+  [orientation directions]
+  (map->Direction
+    {:orientation orientation
+     :position (orientation directions)}))
+
+(defn direction-with-4-sides
+  "Create a direction with 4 sides"
+  [orientation]
+  (create-direction-oriented orientation orientation-sides))
+
+
+
