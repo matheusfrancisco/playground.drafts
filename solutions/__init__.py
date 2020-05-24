92646dd5f9511eb43784bf8f62090627fb8dac0d
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-05-24 18:32:25 -0300

    Add: solution two permutation exer

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,4 +1,5 @@
 from .array_and_strings import is_unique, is_unique_with_counter
+from .array_and_strings import is_string_permutation
 
 
-__all__ = ["is_unique", "is_unique_with_counter"]
+__all__ = ["is_unique", "is_unique_with_counter", "is_string_permutation"]
--- tests/test_array_solutions.py
+++ tests/test_array_solutions.py
@@ -1,4 +1,4 @@
-from solutions import is_unique, is_unique_with_counter
+from solutions import is_unique, is_unique_with_counter, is_string_permutation
 
 
 def test_string_has_all_unique_characters_return_true():
@@ -15,3 +15,11 @@ def test_counter_string_has_all_unique_characters_return_true():
 
 def test_counter_string_has_all_unique_characters_return_false():
     assert is_unique_with_counter("maaatheus") is False
+
+
+def test_should_return_true_if_string2_is_premutation_string1():
+    assert is_string_permutation("matheus", "suehtam") is True
+
+
+def test_should_return_false_if_string2_is_premutation_string1():
+    assert is_string_permutation("matheus", "sam") is False
