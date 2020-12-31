9121c83b3efe58042ba4a5884dd813e7fb6f33ac
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-12-31 20:07:41 -0300

    Solve valid ip addresses

--- py-solution/strings/test_valid_ip_addresses.py
+++ py-solution/strings/test_valid_ip_addresses.py
@@ -23,7 +23,29 @@
 
 
 def valid_ip_addreses(string):
-    return []
+    valids_ips = []
+    for i in range(1, min(len(string), 4)):
+        current_ip_address_parts = ['', '', '', '']
+        current_ip_address_parts[0] = string[:i]
+        if not is_valid_part(current_ip_address_parts[0]):
+            continue
+        for j in range(i + 1, i + min(len(string) - i, 4)):
+            current_ip_address_parts[1] = string[i:j]
+            if not is_valid_part(current_ip_address_parts[1]):
+                continue
+            for k in range(j + 1, j + min(len(string) - j, 4)):
+                current_ip_address_parts[2] = string[j:k]
+                current_ip_address_parts[3] = string[k:]
+                if is_valid_part(current_ip_address_parts[2]) and is_valid_part(current_ip_address_parts[3]): # noqa
+                    valids_ips.append('.'.join(current_ip_address_parts))
+    return valids_ips
+
+
+def is_valid_part(string):
+    string_as_int = int(string)
+    if string_as_int > 255:
+        return False
+    return len(string) == len(str(string_as_int))  # check for leading 0
 
 
 def test_ip_address():
