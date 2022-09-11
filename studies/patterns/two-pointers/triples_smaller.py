66e9761530aea718a5f191cc0bb9981a3beef07d
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-09-11 09:28:10 -0300

    two pointer less than target

---
+++ studies/patterns/two-pointers/triples_smaller.py
@@ -0,0 +1,32 @@
+
+
+def triplet_with_smaller_sum(arr, t):
+  arr.sort()
+  count = 0
+  for i in range(len(arr)-2):
+    count += search_pair(arr, t - arr[i], i)
+
+  return count
+
+
+def search_pair(arr, target_sum, first):
+  count = 0
+  left, right = first + 1, len(arr) - 1
+
+  while(left < right):
+    if arr[left] + arr[right] < target_sum:
+      # since arr[right] >= arr[left], therefore, we can replace arr[right] by any number between
+      # left and right to get a sum less than the target sum
+      count += right - left
+      left += 1
+    else:
+      right -= 1
+  return count
+
+
+def main():
+  print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
+  print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
+
+
+main()
