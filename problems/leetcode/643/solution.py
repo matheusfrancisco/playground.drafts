c5524219dc3f7a71d261dbc84d402bc1db9817e1
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-10-14 09:07:30 -0300

    add leetcode

---
+++ problems/leetcode/643/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        max_avg = float('-inf')
+        start = 0
+        sum_ = 0
+        for idx, n in enumerate(nums):
+            sum_ += n 
+            if idx - start + 1 >= k:
+                avg = sum_/ (idx - start + 1)
+                max_avg = max_(max_avg, avg)
+                sum_ -= nums[start]
+                start += 1
+                
+        return max_avg
+      
+def max_(a, b):
+  if a >= b:
+    return a
+  return b
