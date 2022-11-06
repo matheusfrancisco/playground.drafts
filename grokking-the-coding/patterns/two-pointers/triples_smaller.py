bb989cffab78bbd6c0858f956bae209b38dbc354
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-11-06 17:39:08 -0300

    add and edit some patterns

--- grokking-the-coding/patterns/two-pointers/triples_smaller.py
+++ grokking-the-coding/patterns/two-pointers/triples_smaller.py
@@ -1,32 +1,30 @@
 
 
 def triplet_with_smaller_sum(arr, t):
-  arr.sort()
-  count = 0
-  for i in range(len(arr)-2):
-    count += search_pair(arr, t - arr[i], i)
+    arr.sort()
+    count = 0
+    for i in range(len(arr)-2):
+        count += search_pair(arr, t - arr[i], i)
 
-  return count
+    return count
 
 
 def search_pair(arr, target_sum, first):
-  count = 0
-  left, right = first + 1, len(arr) - 1
+    count = 0
+    left, right = first + 1, len(arr) - 1
 
-  while(left < right):
-    if arr[left] + arr[right] < target_sum:
-      # since arr[right] >= arr[left], therefore, we can replace arr[right] by any number between
-      # left and right to get a sum less than the target sum
-      count += right - left
-      left += 1
-    else:
-      right -= 1
-  return count
+    while(left < right):
+        if arr[left] + arr[right] < target_sum:
+            count += right - left
+            left += 1
+        else:
+            right -= 1
+    return count
 
 
 def main():
-  print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
-  print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
+    print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
+    print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
 
 
 main()
